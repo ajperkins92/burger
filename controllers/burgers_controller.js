98bdfcb50fdaec2a1d03fa0bea82d97a1e3032fc
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-11-26 18:32:58 -0800

    All files added

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    cat.all(function (data) {
+        var hbsObject = {
+            cats: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,12 @@
+var orm = require("./config/orm.js");
+
+// Calls the select all  function
+orm.selectAll("","","");
+
+// 
+orm.insertOne("","","");
+
+// 
+orm.updateOne("","","");
+
+module.exports = burger;
