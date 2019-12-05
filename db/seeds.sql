d0abec3e8527dd5623b93223d1b01db8e00d796a
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-12-05 01:22:41 -0800

    App is displaying data from the database, but post and update requests aren't quite working yet. Styles not yet added. Everything else more or less up-to-date

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tavern Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Portobella Burger');
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,60 +1,45 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-    $(".change-sleep").on("click", function (event) {
-        var id = $(this).data("id");
-        var newSleep = $(this).data("newsleep");
 
-        var newSleepState = {
-            sleepy: newSleep
-        };
 
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: newSleepState
-        }).then(
-            function () {
-                console.log("changed sleep to", newSleep);
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
+    $(function () {
+        $(".change-devoured").on("click", function (event) {
+            var id = $(this).data("id");
+            // var newDevoured = $(this).data("newDevoured");
 
-    $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
+            // var newDevouredState = {
+            //     devoured: newDevoured
+            // };
 
-        var newBurger = {
-            name: $("#ca").val().trim(),
-            sleepy: $("[name=sleepy]:checked").val().trim()
-        };
+            // Send the PUT request.
+            $.ajax("/api/burgers/" + id, {
+                type: "PUT",
+                // data: newDevouredState
+            }).then(
+                function () {
+                    location.reload();
+                }
+            );
+        });
 
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function () {
-                console.log("created new cat");
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
+        $(".create-form").on("submit", function (event) {
+            // Make sure to preventDefault on a submit event.
+            event.preventDefault();
+            console.log("New burger created");
 
-    $(".delete-cat").on("click", function (event) {
-        var id = $(this).data("id");
+            var newBurger = {
+                name: $("#newBurger").val().trim(),
+            };
 
-        // Send the DELETE request.
-        $.ajax("/api/burgers/" + id, {
-            type: "DELETE"
-        }).then(
-            function () {
-                console.log("deleted cat", id);
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
+            // Send the POST request.
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(
+                function () {
+                    location.reload();
+                }
+            );
+        });
     });
 });
