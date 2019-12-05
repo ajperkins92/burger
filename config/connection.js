2618dbf57657de8f0e2391bd95e9df776f5ed80f
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-12-04 19:47:27 -0800

    More tooling about

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 var mysql = require("mysql");
 
+// Set up MySQL connection.
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "7jlmkkAe!",
+  password: "",
   database: "burgers_db"
 });
 
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -16,4 +18,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for ORM.
 module.exports = connection;
