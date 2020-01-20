749685e0e141659a5b93768c38da36fc4ea733b9
Alex Perkins <alex.jakob.perkins@gmail.com>
2020-01-19 22:45:58 -0800

    Big changes

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 
+
 // Set up MySQL connection.
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: process.env.DB_PASS,
   database: "burgers_db"
 });
 
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // Setting Express variables
 var express = require("express");
 var PORT = process.env.PORT || 8080;
+
 var app = express();
 var path = require("path");
+
+// Setting .env variables
+require('dotenv').config();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
