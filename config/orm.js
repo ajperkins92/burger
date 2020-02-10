f2fedaddbbc841c405275aae2af3c85c28666576
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-09 21:44:33 -0800

    Edits orm.js put route

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,4 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
-
+var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
   var arr = [];
@@ -10,19 +8,16 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
-
   for (var key in ob) {
     arr.push(key + "=" + ob[key]);
   }
   return arr.toString();
 }
 
-// Object for all our SQL statement functions.
 var orm = {
-  all: function (tableInput, cb) {
+  all: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
@@ -32,8 +27,7 @@ var orm = {
     });
   },
 
-
-  create: function (table, cols, vals, cb) {
+  create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -45,16 +39,15 @@ var orm = {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function (err, result) {
+    connection.query(queryString, vals, function(err, result) {
       if (err) {
         throw err;
       }
-
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function (table, objColVals, condition, cb) {
+
+  update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -63,14 +56,13 @@ var orm = {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function (err, result) {
+    connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
-  },
+  }
 };
 
-// Export the orm object for the model (cat.js).
 module.exports = orm;
