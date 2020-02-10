536caf17ca1d84b2606899b14d90dca25b9f9bb6
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-09 21:42:11 -0800

    Small edit to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,33 +1,24 @@
-// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function (cb) {
-    orm.all("burgers", function (res) {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
-
-  create: function (cols, vals, cb) {
+  create: function(name, cb) {
     orm.create("burgers", [
-      "burger_name", "devoured"], [
+      "burger_name", "devoured"
+    ], [
       name, false
     ], cb);
   },
-
-  update: function (id, cb) {
-    var condtiion = "id=" + id;
+  update: function(id, cb) {
+    var condition = "id=" + id;
     orm.update("burgers", {
       devoured: true
     }, condition, cb);
-  },
-
-  delete: function (condition, cb) {
-    orm.all("burgers", objColVals, condition, function (res) {
-      cb(res);
-    });
   }
 };
 
-// Export the database functions for the controller (burgers_Controller.js).
 module.exports = burger;
