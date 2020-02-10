aa2dc05b78b9a4f9a56600e881c985309d28a561
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-09 20:46:04 -0800

    Modifies burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,50 +3,49 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-  burger.selectAll(function (data) {
-    var hdbrsObj = {
-      burgers: data
-    };
-    console.log(hdbrsObj);
-    res.render("index", hdbrsObj)
-  });
+  res.redirect("/burgers");
+});
 
-
-  router.post("/api/burgers/", function (req, res) {
-    burger.insertOne(
-      ["burger_name", "devoured"], [req.body.burger_name, req.body.devoured],
-      function (result) {
-        // Send back the ID of the new quote
-        res.json({ id: result.insertId });
-      });
-  });
-
-  router.put("/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition: ", condition);
-    burger.updateOne(
-      { devoured: req.body.devoured }, condition, function (result) {
-        if (result, changedRows === 0) {
-          return res.status(404).end();
-        } else {
-          res.status(200);
-        }
-      });
-  });
-
-  router.delete("burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition: ", condition);
-
-    burger.deleteOne(condition, function (result) {
-      if ((result, changedRows === 0)) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+router.get("/burgers", function (req, res) {
+  burger.all(function (burgerData) {
+    res.render("index", { burger_data: burgerData });
   });
 });
+
+
+router.post("/burgers/create/", function (req, res) {
+  burger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+router.put("/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition: ", condition);
+  burger.updateOne(
+    { devoured: req.body.devoured }, condition, function (result) {
+      if (result, changedRows === 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200);
+      }
+    });
+});
+
+router.delete("burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition: ", condition);
+
+  burger.deleteOne(condition, function (result) {
+    if ((result, changedRows === 0)) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
 
 
 // Export routes for server.js to use.
