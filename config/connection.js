23ba1dbed7c8a5de0e3ec3c76d2597c9c3877a04
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-09 22:43:19 -0800

    Configures connection to work with Heroku MySQL deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+var connection;
 
-connection.connect(function(err) {
+// Configured to work with JAWSDB for Heroku deployment
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+};
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
